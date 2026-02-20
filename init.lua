3430c862f5c166784b01ba403061573cb8675bab
Myroslav Repin <myroslavrepin@gmail.com>
2026-02-20 00:33:35 -0700

    fixed all visible config errors and ready to be used

---
+++ init.lua
@@ -0,0 +1,33 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+-- Lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Loading plugins conf
+require("lazy").setup("plugins")
+
+-- Loading tressitter
+--local ok, config = pcall(require, "nvim-treesitter.configs")
+--if ok then
+--  config.setup({
+--    ensure_installed = {"lua", "javascript"},
+--    highlight = { enabled = true },
+--    indent = { enabled = true }
+--  })
+--end
+
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+    lazy = false,
+    config = function()
+      require("nvim-treesitter").install({ "lua", "javascript" })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "lua", "javascript" },
+        callback = function()
+          vim.treesitter.start()
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
+  },
+}
